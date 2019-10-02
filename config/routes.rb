37c75bb34d8851dadad82a84e8e36c14a5b2c368
hikaru.kawauchi <hikaru.kawauchi@gmail.com>
2019-10-02 05:55:59 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   root to: 'tasklists#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 
   resources :tasklists
 end
